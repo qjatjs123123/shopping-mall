71c001767c43226abd65be5b5d7db2e1cc6e6889
홍범선 <qjatjs123123@naver.com>
2024-01-19 15:57:46 +0900

    refactor: 디자인 변경

--- src/components/Main/MainImgContainer.tsx
+++ src/components/Main/MainImgContainer.tsx
@@ -6,6 +6,7 @@ export default function MainImgContainer() {
     <>
       <div className='MainImgContainer'>
         <img src='https://atimg.sonyunara.com/files/attrangs/new_banner/1703232790_0.jpg'></img>
+        {/* <img src='https://atimg.sonyunara.com/files/attrangs/new_banner/1705463928_0.jpg'></img> */}
       </div>
     </>
   );
