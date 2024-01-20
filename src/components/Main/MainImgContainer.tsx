277baa265d9bfd5819ed550deb01a221204334d2
Hong Boem Sun <qjatjs123123@naver.com>
2024-01-20 18:31:25 +0900

    feat: 5초마다 무한 이미지 슬라이더 기능 구현

--- src/components/Main/MainImgContainer.tsx
+++ src/components/Main/MainImgContainer.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useState,useEffect,useRef  } from "react";
 import "./MainImgContainer.css";
 import "../../responsive.css";
 
 export default function MainImgContainer() {
   const [imgIdx, setimgIdx] = useState(1);
   const [flg, setFlg] = useState(false);
-  const [isTransition, setIsTransition] = useState(false);
-  
+  const isTransitionRef = useRef(false);
+  useEffect(() => {
+    setInterval(() => {
+      if(!isTransitionRef.current) handleMainImg(1);
+    }, 5000);
+  }, [])
+
+
+
   const mainImgList = [
     {
       src: "https://atimg.sonyunara.com/files/attrangs/new_banner/1705387347_1.jpg",
@@ -29,29 +36,34 @@ export default function MainImgContainer() {
   ];
 
   const handleMainImg = (val: number) => {
-    
-    if(isTransition) return;
 
-    if (!isTransition) {
-      setIsTransition(true);
+    if(isTransitionRef.current) return;
+    if (!isTransitionRef.current) {
+      isTransitionRef.current = true;
       setTimeout(() => {
-        setIsTransition(false);
-      }, 500);
+        isTransitionRef.current = false;
+      }, 700);
     }
-    
-    setimgIdx(imgIdx + val);
-    setFlg(false);
-    if (imgIdx + val === 0) {
-      setTimeout(() => {
-        setFlg(true);
-        setimgIdx(4);
-      }, 500);
-    } else if (imgIdx + val === 5) {
-      setTimeout(() => {
-        setFlg(true);
-        setimgIdx(1);
-      }, 500);
-    }
+
+    // 클로저 회피
+    setimgIdx((imgIdx) => {
+      const newIdx = imgIdx + val;
+      setFlg(false);
+  
+      if (newIdx === 0) {
+        setTimeout(() => {
+          setFlg(true);
+          setimgIdx(4);
+        }, 700);
+      } else if (newIdx === 5) {
+        setTimeout(() => {
+          setFlg(true);
+          setimgIdx(1);
+        }, 700);
+      }
+  
+      return newIdx;
+    });
   };
 
   return (
@@ -59,7 +71,7 @@ export default function MainImgContainer() {
       <div className="MainImgContainer">
         <div
           className="MainImgInner"
-          style={{ marginLeft: -(100 * imgIdx) + "vw", transition : flg ? '0.0s' : '0.5s'}}
+          style={{ marginLeft: -(100 * imgIdx) + "vw", transition : flg ? '0.0s' : '0.7s'}}
         >
           {mainImgList.map(({ src }, idx) => (
             <img src={src} key={idx}></img>
