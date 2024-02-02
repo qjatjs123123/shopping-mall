ae760f5ecd4262ef5b6c14c22bc8e9e27c94b44f
Hong Boem Sun <qjatjs123123@naver.com>
2024-02-02 17:41:25 +0900

    feat: 이미지 슬라이더 클릭 무브 이벤트 구현

--- src/components/Sale/MainSaleContainer.tsx
+++ src/components/Sale/MainSaleContainer.tsx
@@ -15,11 +15,16 @@ export default function MainSaleContainer() {
   const [innerWidth, setinnerWidth] = useState(0);
   const [cnt, setCnt] = useState(1);
   const [curMargin, setCurMargin] = useState(0);
-  const [curImgIdx, setCurImgIdx] = useState<number>(3);
+  const [curImgIdx, setCurImgIdx] = useState<number>(1);
   const [saleImgList, setSaleImgList] = useState<SaleImgProps[]>([]);
   const [imgMaxLen, setimgMaxLen] = useState(0);
-  const [flg, setFlg] = useState(false);
   const isTransitionRef = useRef(false);
+  const eventImgContainerParent = useRef<HTMLDivElement>(null);
+  const startPoint = useRef(0);
+  const endPoint = useRef(0);
+  const mouseup = useRef(true);
+  const flg = useRef(false);
+  const [delta, setDelta] = useState(0);
 
   const handleResize = () => {
     setCurMargin(Math.round(document.documentElement.clientWidth / 10));
@@ -27,10 +32,53 @@ export default function MainSaleContainer() {
     if (document.documentElement.clientWidth <= 768) setCnt(1);
     else setCnt(3);
   };
+  useEffect(() => {
+    const element = eventImgContainerParent.current;
+    if (imgMaxLen === 0 || !element) return;
+
+    const handleMouseDown = (e: MouseEvent) => {
+      startPoint.current = e.pageX; // 마우스 드래그 시작 위치 저장
+      mouseup.current = false;
+      flg.current = true;
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (mouseup.current) return;
+      endPoint.current = e.pageX;
+      const tmp = endPoint.current - startPoint.current;
+      if (tmp === 0) return;
+      setDelta(tmp);
+    };
+
+    const handleMouseUp = (e: MouseEvent) => {
+      setDelta(delta * -1);
+      mouseup.current = true;
+      flg.current = false;
+      const tmp = endPoint.current - startPoint.current;
+      const gap = (innerWidth - curMargin * 2) / cnt;
+      let plus = 0;
+      if (tmp < 0) plus = Math.round((tmp * -1) / gap) * -1;
+      else plus = Math.round(tmp / gap);
+      saleImgSlider(plus * -1);
+    };
+
+    element.addEventListener("mousedown", handleMouseDown);
+    element.addEventListener("mousemove", handleMouseMove);
+    element.addEventListener("mouseup", handleMouseUp);
+
+    // 리스너를 제거하는 함수 반환
+    return () => {
+      element.removeEventListener("mousedown", handleMouseDown);
+      element.removeEventListener("mousemove", handleMouseMove);
+      element.removeEventListener("mouseup", handleMouseUp);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [imgMaxLen]);
 
   useEffect(() => {
     setCnt(1);
     handleResize();
+
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
@@ -48,17 +96,16 @@ export default function MainSaleContainer() {
 
     setCurImgIdx((curImgIdx) => {
       const newIdx = curImgIdx + val;
-      console.log(newIdx);
-      setFlg(false);
 
-      if (newIdx === 0) {
+      flg.current = false;
+      if (newIdx <= 0) {
         setTimeout(() => {
-          setFlg(true);
+          flg.current = true;
           setCurImgIdx(imgMaxLen - 6);
         }, 500);
-      } else if (newIdx === imgMaxLen - 3) {
+      } else if (newIdx >= imgMaxLen - 3) {
         setTimeout(() => {
-          setFlg(true);
+          flg.current = true;
           setCurImgIdx(3);
         }, 500);
       }
@@ -71,7 +118,7 @@ export default function MainSaleContainer() {
     if (curImgIdx > imgMaxLen - 6) return curImgIdx - (imgMaxLen - 6);
     else if (curImgIdx === 0) return imgMaxLen - 6;
     return curImgIdx;
-  }
+  };
 
   return (
     <>
@@ -86,25 +133,27 @@ export default function MainSaleContainer() {
         <div className="MainSaleTitle">
           <h2>이벤트</h2>
           <div className="PageNumberContainer">
-            {pageNumberHandler()} / 
-            <span> {imgMaxLen - 6}</span>
+            {pageNumberHandler()} /<span> {imgMaxLen - 6}</span>
           </div>
         </div>
-        
+
         <div className="ButtonContainer">
           <LeftButtonArrow handleImgSlide={saleImgSlider} />
           <RightButtonArrow handleImgSlide={saleImgSlider} />
-          <div className="eventImgContainerParent">
+          <div
+            ref={eventImgContainerParent}
+            className="eventImgContainerParent"
+          >
             <div
               className="eventImgContainer"
               style={{
                 width: `calc((${innerWidth}px - ${
                   curMargin * 2
                 }px) / ${cnt} * ${saleImgList.length})`,
-                marginLeft: `calc((${innerWidth}px - ${
+                marginLeft: `calc(((${innerWidth}px - ${
                   curMargin * 2
-                }px) / ${cnt} * ${curImgIdx}*-1)`,
-                transition: flg ? "0.0s" : "0.5s",
+                }px) / ${cnt} * ${curImgIdx}*-1) + ${delta}px)`,
+                transition: flg.current ? "0.0s" : "0.5s",
               }}
             >
               {saleImgList.map(({ src, txt }, idx) => (
@@ -117,12 +166,20 @@ export default function MainSaleContainer() {
                     }px) / ${cnt})`,
                   }}
                 >
-                  <img src={src} alt=""></img>
-                  <div style={{
-                    width: `calc(((${innerWidth}px - ${
-                      curMargin * 2
-                    }px) / ${cnt}) - 20px)`,
-                  }}>{txt}</div>
+                  <img
+                    src={src}
+                    alt=""
+                    onMouseDown={(e) => e.preventDefault()}
+                  ></img>
+                  <div
+                    style={{
+                      width: `calc(((${innerWidth}px - ${
+                        curMargin * 2
+                      }px) / ${cnt}) - 20px)`,
+                    }}
+                  >
+                    {txt}
+                  </div>
                 </div>
               ))}
             </div>
